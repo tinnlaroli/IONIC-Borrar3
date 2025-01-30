eb3783b1cfd837c0d524d59c0baded36c2d06f93
Martin <olivarezmartin714@gmail.com>
2025-01-30 08:34:18 -0600

    chore: borrar carpetas tab

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -11,14 +11,15 @@ const routes: Routes = [
         path: 'feed',
         loadChildren: () => import('../feed/feed.module').then(m => m.FeedPageModule)
       },
-      {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
-      },
-      {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-      },
+      //rutas de las paginas
+      // {
+      //   path: 'tab2',
+      //   loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+      // },
+      // {
+      //   path: 'tab3',
+      //   loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+      // },
       {
         path: '',
         redirectTo: '/feed',
