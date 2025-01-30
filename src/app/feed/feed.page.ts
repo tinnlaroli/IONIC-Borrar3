55f9dae00962e72caa646a4c71480cf3bc8e66aa
Martin <olivarezmartin714@gmail.com>
2025-01-30 08:15:13 -0600

    first commit

---
+++ src/app/feed/feed.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feed',
+  templateUrl: './feed.page.html',
+  styleUrls: ['./feed.page.scss'],
+  standalone: false
+})
+export class FeedPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
